63664265a8f6c1e0b8078158512591c5ac0c3d13
anderson <anderson.hamamoto@gmail.com>
2013-08-21 00:40:37 -0300

    RmiServer.java adicionado

---
+++ examples/RmiServer.java
@@ -0,0 +1,32 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.*;
+
+public class RmiServer extends UnicastRemoteObject implements RmiServerIntf {
+	public static final String MESSAGE = "Hello World!";
+	
+	public RmiServer() throws RemoteException {
+		super(0);
+	}
+	
+	public String getMessage() {
+		return MESSAGE;
+	}
+	
+	public static void main(String args[]) throws Exception {
+		System.out.println("RMI Server started.");
+		
+		try {
+			LocateRegistry.createRegistry(1099);
+			System.out.println("RMI registry started");
+		} catch {
+			System.out.println("RMI registry already exists");
+		}
+		
+		RmiServer obj = new RmiServer();
+		
+		Naming.rebind("//localhost/RmiServer", obj);
+		System.out.println("PeerServer bound in registry");
+	}
+}
