e53dc3bb2ab365a18725cf9146e1697b8ce61fd1
anderson <anderson.hamamoto@gmail.com>
2013-08-21 17:32:11 -0300

    mudei o esquema do whiteboard, agora ele implementa MouseListener

--- examples/Whiteboard.java
+++ examples/Whiteboard.java
@@ -4,22 +4,32 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Whiteboard {
+public class Whiteboard implements MouseListener {
 	private static JFrame frame;
 	private static int W = 600, H = 480;
 	private int last_x;
 	private int last_y;
 	
-	MouseListener getCoords = new MouseAdapter() {
-		public void mouseClicked(MouseEvent m) {
-			int x = m.getX();
-			int y = m.getY();
-			Graphics g = frame.getGraphics();
-			g.drawLine(last_x, last_y, x, y);
-			last_x = x;
-			last_y = y;
-		}
-	};
+	public void mousePressed(MouseEvent m) {	
+	}
+	
+	public void mouseReleased(MouseEvent m) {
+	}
+	
+	public void mouseEntered(MouseEvent m) {
+	}
+	
+	public void mouseExited(MouseEvent m) {
+	}
+	
+	public void mouseClicked(MouseEvent m) {
+		int x = m.getX();
+		int y = m.getY();
+		Graphics g = frame.getGraphics();
+		g.drawLine(last_x, last_y, x, y);
+		last_x = x;
+		last_y = y;
+	}
 	
 	public Whiteboard() {
 		this.last_x = 2;
@@ -28,7 +38,7 @@ public class Whiteboard {
 		frame.setSize(W, H);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.addMouseListener(getCoords);
+		frame.addMouseListener(this);
 	}
 	
 	public static void main(String args[]) {
