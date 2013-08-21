dce0f14652d0a5f730228de8c19921625070299f
anderson <anderson.hamamoto@gmail.com>
2013-08-21 08:36:49 -0300

    javadoc no RmiClient

--- examples/RmiClient.java
+++ examples/RmiClient.java
@@ -1,6 +1,12 @@
 import java.rmi.Naming;
 
+/**
+	* Classe do cliente que simplesmente manda a requisição e imprime uma mensagem.
+	*/
 public class RmiClient {
+	/**
+		* Método main que cria o objeto e manda a requisição.
+		*/
 	public static void main(String args[]) throws Exception {
 		RmiServerIntf obj = (RmiServerIntf)Naming.lookup("//localhost/RmiServer");
 		System.out.println(obj.getMessage());
