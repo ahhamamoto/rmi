d5a410b7317b48fc1f9e35b8ace806b3d0a5612b
anderson <anderson.hamamoto@gmail.com>
2013-08-21 08:36:30 -0300

    javadoc no RmiServerIntf

--- examples/RmiServerIntf.java
+++ examples/RmiServerIntf.java
@@ -1,6 +1,9 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+	* Classe que tem somente a interface que o servidor implementa do RMI.
+	*/
 public interface RmiServerIntf extends Remote {
 	public String getMessage() throws RemoteException;
 }
